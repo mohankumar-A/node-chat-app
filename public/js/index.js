b23a3b643e4eb1fd97f63a5f619d9c7403755d33
Mohan Kumar <sweetperson.mk@gmail.com>
2017-07-23 17:28:46 +0530

    added auto scroll into the messages div

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom(){
+  var messages = $("#messages");
+  var newMessage = messages.children("li:last-child");
+  var clientHeight = messages.prop("clientHeight");
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+      messages.scrollTop(scrollHeight);
+  }
+
+};
+
+
 socket.on("connect", function() {
     console.log("connected to server");
 });
@@ -14,6 +31,7 @@ socket.on("newMessage", function(message){
        text: message.text
     });
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 
